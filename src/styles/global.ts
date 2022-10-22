700c207a59c2889fcb7fc63b1d86af45a88864cc
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-10-22 17:21:41 -0300

    feat: Styles global created

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.base.text};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font-family: ${({ theme }) => theme.fontFamily.text}, sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`;
