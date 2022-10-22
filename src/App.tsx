22c0ad40d44f697b8dc9525e75dfca89b115ef24
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-10-22 17:53:09 -0300

    feat: Default layout created

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
-import { Router } from './Routes'
+import { Router } from './Router'
 
 export function App() {
   return (
